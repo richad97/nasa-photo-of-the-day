8222331846d41ce56f38219c9ea6b28413a171c1
richad.97 <rcastillo2022@gmail.com>
2021-11-03 13:56:24 -0700

    called api for nasa apod

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=q6ts81N1nnTcUmz3h9d0qarPtZAi4VyiVsDPZLGq"
+      )
+      .then((resp) => {
+        const data = resp.data;
+
+        setPhoto(data.url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
+      <img src={photo} alt="nasa-apod" width="500" height="500"></img>
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
